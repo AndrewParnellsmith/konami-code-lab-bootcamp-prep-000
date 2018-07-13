81914e4e1e9c62285d66514e5da7da224a40f78c
Andrew Parnell-Smith <andrewparnellsmith@gmail.com>
2018-07-13 05:08:16 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,7 +12,22 @@ const codes = [
 ];
 
 function init() {
+
 let index = 0;
 
-
+codes.addEventListener('keydown', function onKeyDownHandler(e) {
+  const key = e.key;
+ 
+  if (key === codes[index]) {
+    index++;
+ 
+    if (index === codes.length) {
+      alert("Finish Him!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+})
 }
